a8fec54d52200d935b5440a21c47c7458b5d0230
VishalMCF <vishaly650@gmail.com>
2022-05-09 13:48:41 +0530

    Initial Commit.
    Initial Commit.

---
+++ backend/microservice-1-course/src/main/java/com/example/redcat/microservice1course/Microservice1CourseApplication.java
@@ -0,0 +1,13 @@
+package com.example.redcat.microservice1course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Microservice1CourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Microservice1CourseApplication.class, args);
+	}
+
+}
